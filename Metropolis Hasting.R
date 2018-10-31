455e3cc41eef178727a92eb6557ee454b1693e4a
Yuhan Li <liyuhan529@gmail.com>
2018-10-30 19:57:00 -0500

    codes for metropolis hasting

---
+++ Metropolis Hasting.R
@@ -0,0 +1,32 @@
+mh.beta <- function(iterations, start,shape1, shape2,c) {
+  theta.old <- start
+  draws <- c()
+  theta.update <- function(theta.old, shape1, shape2) {
+    theta.new <- rbeta(1, c*theta.old, c*(1-theta.old))
+    accept.prob <- dbeta(theta.new, shape1 = shape1, shape2=shape2)/dbeta(theta.old,shape1 = shape1, shape2 = shape2)
+    if (runif(1) <= accept.prob) 
+    {theta.update=theta.new} 
+    else 
+        {theta.update=theta.old}
+  }
+  for (i in 1:iterations) {
+    draws[i] <- theta.old <- theta.update(theta.old, shape1 = shape1,
+                                          shape2 = shape2)
+  }
+  return(draws[1:iterations])
+}
+
+
+draws <- mh.beta(10000, start = runif(1),shape1 = 6, shape2 = 4,c=1)
+
+par(mfrow=c(1,3))  #1 row, 3 columns
+plot(draws); 
+acf(draws);
+hist(draws)  #plot commands
+
+
+
+
+
+
+
